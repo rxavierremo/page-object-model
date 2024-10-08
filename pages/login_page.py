c45f36d4a356a8f010b6978d992af4306e28c9ac
Ranielo Xavier Remo <ranieloremo@gmail.com>
2024-10-08 20:11:38 +0800

    Updated LoginPage functions

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,3 +8,15 @@ class LoginPage:
         self.username_textbox = (By.ID, "#")            # Change username tag if needed
         self.password_textbox = (By.ID, "#")            # Change password tag if needed
         self.login_button = (By.XPATH, "#")             # Change login_button tag if needed
+
+    def open_webpage(self, url):
+        self.driver.get(url)
+
+    def input_username(self, username):
+        self.driver.find_element(self.username_textbox).send_keys(username)
+
+    def input_password(self, password):
+        self.driver.find_element(self.password_textbox).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(self.login_button).click()
