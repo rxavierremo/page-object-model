537ca4249433c560c128348e9abeda1e9d583733
Ranielo Xavier Remo <ranieloremo@gmail.com>
2024-10-10 21:50:30 +0800

    Updated login_page.py

--- tests/test_orangehrm_login_page.py
+++ tests/test_orangehrm_login_page.py
@@ -1,8 +1,8 @@
 import pytest
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
 from login_page import LoginPage
 
 
