9c6b664c9352cd2880d21f79b3d3256f8ddae566
Ranielo Xavier Remo <ranieloremo@gmail.com>
2024-10-10 19:33:40 +0800

    Updated tests directory

--- tests/login_page.py
+++ tests/login_page.py
@@ -1,22 +1,22 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 
 class LoginPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.username_textbox = (By.XPATH, "//input[@placeholder='Username']")            # Change username tag if needed
-        self.password_textbox = (By.XPATH, "//input[@placeholder='Password']")            # Change password tag if needed
-        self.login_button = (By.XPATH, "//button[normalize-space()='Login']")             # Change login_button tag if needed
+        self.username_textbox = (By.CSS_SELECTOR, "input[placeholder='username']")            # Change username tag if needed
+        self.password_textbox = (By.CSS_SELECTOR, "input[placeholder='password']" )            # Change password tag if needed
+        self.login_button = (By.CSS_SELECTOR, "button[type='submit']")             # Change login_button tag if needed
 
     def open_webpage(self, url):
         self.driver.get(url)
 
     def input_username(self, username):
-        self.driver.find_element(self.username_textbox).send_keys(username)
+        self.driver.find_element(*self.username_textbox).send_keys(username)
 
     def input_password(self, password):
-        self.driver.find_element(self.password_textbox).send_keys(password)
+        self.driver.find_element(*self.password_textbox).send_keys(password)
 
     def click_login(self):
-        self.driver.find_element(self.login_button).click()
+        self.driver.find_element(*self.login_button).click()
