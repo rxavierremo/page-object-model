262bbb997e25a905a41a60b870e7086ebe639f3b
Ranielo Xavier Remo <ranieloremo@gmail.com>
2024-10-10 21:45:05 +0800

    Updated login_page.py

--- tests/test_orangehrm_login_page.py
+++ tests/test_orangehrm_login_page.py
@@ -5,28 +5,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from login_page import LoginPage
-=======
+
 from login_page import LoginPage
 import time
 from selenium import webdriver
->>>>>>> 23361caa604fdfd423541970d16f9b977085b42f
+
 
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
-<<<<<<< HEAD
+
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
     driver.close()
     # driver.quit()
-=======
+
     driver.implicitly_wait(5)
     driver.maximize_window()
     yield driver
     driver.close()
     driver.quit()
->>>>>>> 23361caa604fdfd423541970d16f9b977085b42f
 
 def test_login(driver):
     login_page = LoginPage(driver)
@@ -37,7 +36,6 @@ def test_login(driver):
     login_page.input_password("admin123")
     time.sleep(1)
     login_page.click_login()
-<<<<<<< HEAD
     time.sleep(1)
 
     assert "Successful" in driver.page_source
@@ -45,10 +43,3 @@ def test_login(driver):
 
 
 
-
-
-
-
-=======
-    time.sleep(1)
->>>>>>> 23361caa604fdfd423541970d16f9b977085b42f
